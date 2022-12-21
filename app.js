764c85650af6808e1eb00c30835b02cf9cd012f8
muratozkan79 <muratozkan79@gmail.com>
2022-12-21 14:12:08 +0300

    files added

---
+++ app.js
@@ -0,0 +1,14 @@
+//? ************AVERAGE NUMBER***********
+
+let numberToplam = +prompt("Toplamda kaç sayının ortalamasını alacaksınız?:");
+let number1 = +prompt("1. Sayıyı giriniz:");
+let number2 = +prompt("2. Sayıyı giriniz:");
+let number3 = +prompt("3. Sayıyı giriniz:");
+
+alert(
+  `Bu ${numberToplam} sayının ortalaması:
+  ${(number1 + number2 + number3) / numberToplam}`
+);
+// alert(`${number1 + number2}`);
+
+//? ************AVERAGE NUMBER***********
